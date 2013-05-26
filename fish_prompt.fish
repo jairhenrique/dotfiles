63df99ec6c7bf67973c5cd778c325a7befed93e3
Jair Henrique <jair.henrique@gmail.com>
2013-05-26 19:52:58 -0300

    Improviments

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,28 +2,38 @@
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_branch white
  
 # Status Chars
-set __fish_git_prompt_char_dirtystate ' ✱'
+set __fish_git_prompt_char_dirtystate '±'
+set __fish_git_prompt_char_cleanstate ''
 set __fish_git_prompt_char_stagedstate ' →'
 set __fish_git_prompt_char_stashstate ' ↩'
 set __fish_git_prompt_char_upstream_ahead ' ↑'
 set __fish_git_prompt_char_upstream_behind ' ↓'
- 
+
+function my_prompt_pwd --description 'Print the current working directory'
+  echo $PWD | sed -e "s|^$HOME|~|"
+end
+
 function fish_prompt
-  set last_status $status
- 
   set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
+  printf '%s' (my_prompt_pwd)
   set_color normal
  
   printf '%s ' (__fish_git_prompt)
- 
   set_color normal
 end
 
 function fish_right_prompt -d "Write out the right prompt"
-    set_color green
-    date "+%d-%m-%y %H:%M"
+  set -l last_status $status
+  
+  set_color green
+  
+  if test $last_status -ne 0
+    set_color red
+  end
+  
+  date "+%d-%m-%y %H:%M"
+  set color normal
 end
