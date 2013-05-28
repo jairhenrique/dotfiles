bd29579104013591243cfe9d9962fb97a86667fd
Jair Henrique <jair.henrique@gmail.com>
2013-05-27 21:23:26 -0300

    melhorias

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,15 +2,22 @@
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch white
+set __fish_git_prompt_showuntrackedfiles 'yes'
+
+# Git colors
+set __fish_git_prompt_color_branch red
+set __fish_git_prompt_color_dirtystate yellow
+set __fish_git_prompt_color_untrackedfiles cyan
  
 # Status Chars
-set __fish_git_prompt_char_dirtystate '±'
-set __fish_git_prompt_char_cleanstate ''
-set __fish_git_prompt_char_stagedstate ' →'
-set __fish_git_prompt_char_stashstate ' ↩'
-set __fish_git_prompt_char_upstream_ahead ' ↑'
-set __fish_git_prompt_char_upstream_behind ' ↓'
+set __fish_git_prompt_char_upstream_equal ''
+set __fish_git_prompt_char_untrackedfiles ' ✚ '
+set __fish_git_prompt_char_dirtystate ' ☂ '
+set __fish_git_prompt_char_stagedstate ' → '
+set __fish_git_prompt_char_stashstate ' ↩ '
+set __fish_git_prompt_char_upstream_ahead ' ↑ '
+set __fish_git_prompt_char_upstream_behind ' ↓ '
+
 
 function my_prompt_pwd -d 'Print the current working directory'
   echo $PWD | sed -e "s|^$HOME|~|"
@@ -35,6 +42,6 @@ function fish_right_prompt -d "Write out the right prompt"
     set_color red
   end
   
-  date "+%d-%m-%y %H:%M"
+  date "+%H:%M:%S"
   set color normal
 end
